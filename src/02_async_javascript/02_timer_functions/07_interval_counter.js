019dd4cee037b59cace001e9e34b7ab368b6a34f
Adam Cygan <adam.cygan@onet.eu>
2025-11-08 09:23:46 +0100

    feat: added timer functions

--- src/02_async_javascript/02_timer_functions/07_interval_counter.js
+++ src/02_async_javascript/02_timer_functions/07_interval_counter.js
@@ -7,7 +7,21 @@ The function should use setInterval to print integers.
 If n is less than 1, the function should immediately print "Invalid input".
 */
 function startCounter(n, delay) {
-    // Implement this function
+  if (n < 1) {
+    console.log('Invalid input');
+    return;
+  }
+
+  let counter = 1;
+
+  const intervalId = setInterval(() => {
+    console.log(counter);
+    counter++;
+
+    if (counter > n) {
+      clearInterval(intervalId);
+    }
+  }, delay);
 }
 
 module.exports = startCounter;
